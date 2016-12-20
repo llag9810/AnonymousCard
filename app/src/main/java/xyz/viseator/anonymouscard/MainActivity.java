e0bac1985f51a0f53f19978b6612a31b4f886581
yanhao <254836360@qq.com>
2016-12-20 22:06:58 +0800

    完成局域网下多设备通讯

--- app/src/main/java/xyz/viseator/anonymouscard/MainActivity.java
+++ app/src/main/java/xyz/viseator/anonymouscard/MainActivity.java
@@ -1,13 +1,57 @@
 package xyz.viseator.anonymouscard;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import xyz.viseator.anonymouscard.network.ComUtil;
 
+public class MainActivity extends AppCompatActivity
+                            implements View.OnClickListener{
+    private Button mbuttonSend,mButtonJoin;
+    private EditText editText;
+    private TextView textView;
+    private ComUtil comUtil=null;
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case ComUtil.BROADCAST_PORT:
+                    String str=(String)msg.obj;
+                    Toast.makeText(MainActivity.this,str,Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mbuttonSend=(Button)findViewById(R.id.send_msg);
+        mbuttonSend.setOnClickListener(this);
+        mButtonJoin=(Button)findViewById(R.id.join_group);
+        mButtonJoin.setOnClickListener(this);
+        editText=(EditText)findViewById(R.id.edit_msg);
+        textView=(TextView)findViewById(R.id.show_rec_content);
+        comUtil=new ComUtil(handler);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.send_msg:
+                comUtil.broadCast("hello world");
+                break;
+            case R.id.join_group:
+                comUtil.startRecieveMsg();
+                break;
+        }
     }
 }
