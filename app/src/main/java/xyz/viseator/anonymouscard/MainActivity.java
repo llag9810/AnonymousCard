bdf530206be1500a82d8cdb0bf4f858c47a75a3f
viseator <viseator@gmail.com>
2016-12-22 14:11:21 +0800

    Ready to merge

--- app/src/main/java/xyz/viseator/anonymouscard/MainActivity.java
+++ app/src/main/java/xyz/viseator/anonymouscard/MainActivity.java
@@ -1,9 +1,9 @@
 package xyz.viseator.anonymouscard;
 
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,11 +11,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import xyz.viseator.anonymouscard.data.ConvertData;
@@ -29,13 +24,13 @@ public class MainActivity extends AppCompatActivity
         implements View.OnClickListener {
     private static final String TAG = "wudi MainActivity";
     private DataPackage dataPackage;
-    private Button mbuttonSend, mButtonJoin,mButtonSendSingle;
+    private Button mbuttonSend, mButtonJoin, mButtonSendSingle;
     private EditText editText;
     private TextView textViewShowIP, textViewShowMac, textViewTitle;
     private ComUtil comUtil = null;
     private ArrayList<String> receivedIds;
-    private SingleUtil singleUtil=null;
-    private static String testIP=null;
+    private SingleUtil singleUtil = null;
+    private static String testIP = null;
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -43,18 +38,18 @@ public class MainActivity extends AppCompatActivity
             switch (msg.what) {
                 case ComUtil.BROADCAST_PORT:
                     byte[] data = (byte[]) msg.obj;
-                    Toast.makeText(MainActivity.this, "Received", Toast.LENGTH_SHORT).show();
                     UDPDataPackage udpDataPackage = ConvertData.ByteToDataPackage(data);
                     if (!receivedIds.contains(udpDataPackage.getId())) {
+                        Toast.makeText(MainActivity.this, "Received", Toast.LENGTH_SHORT).show();
                         receivedIds.add(udpDataPackage.getId());
                         textViewTitle.setText(udpDataPackage.getTitle());
                         textViewShowMac.setText(udpDataPackage.getMacAddress());
                         textViewShowIP.setText(udpDataPackage.getIpAddress());
-                        testIP=udpDataPackage.getIpAddress();
+                        testIP = udpDataPackage.getIpAddress();
                     }
                     break;
                 case SingleUtil.SINGLE_PORT:
-                    Toast.makeText(MainActivity.this,"收到私有信息",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "收到私有信息", Toast.LENGTH_SHORT).show();
                     break;
             }
         }
@@ -69,7 +64,7 @@ public class MainActivity extends AppCompatActivity
         mbuttonSend.setOnClickListener(this);
         mButtonJoin = (Button) findViewById(R.id.join_group);
         mButtonJoin.setOnClickListener(this);
-        mButtonSendSingle=(Button)findViewById(R.id.send_single);
+        mButtonSendSingle = (Button) findViewById(R.id.send_single);
         mButtonSendSingle.setOnClickListener(this);
         editText = (EditText) findViewById(R.id.edit_msg);
         textViewShowIP = (TextView) findViewById(R.id.show_ip);
@@ -78,7 +73,7 @@ public class MainActivity extends AppCompatActivity
         textViewTitle = (TextView) findViewById(R.id.show_title);
         comUtil = new ComUtil(handler);
         dataPackage = new DataPackage();
-        singleUtil=new SingleUtil(handler);
+        singleUtil = new SingleUtil(handler);
     }
 
     @Override
@@ -101,9 +96,9 @@ public class MainActivity extends AppCompatActivity
                 singleUtil.startRecieveMsg();
                 break;
             case R.id.send_single:
-                String str1=new String("hello world");
-                byte[] strb=str1.getBytes();
-                singleUtil.sendSingle(strb,testIP);
+                String str1 = new String("hello world");
+                byte[] strb = str1.getBytes();
+                singleUtil.sendSingle(strb, testIP);
                 break;
         }
     }
