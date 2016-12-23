1d8c8de9f45f7d45f9cad05ecb6ee921fb499baf
viseator <viseator@gmail.com>
2016-12-23 23:08:24 +0800

    add wudi TCP

---
+++ app/src/main/java/xyz/viseator/anonymouscard/network/TcpClient.java
@@ -0,0 +1,62 @@
+package xyz.viseator.anonymouscard.network;
+
+import android.os.Handler;
+import android.os.Message;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+import xyz.viseator.anonymouscard.data.DataPackage;
+import xyz.viseator.anonymouscard.data.UDPDataPackage;
+
+/**
+ * Created by viseator on 2016/12/23.
+ * Wudi
+ * viseator@gmail.com
+ */
+
+public class TcpClient {
+    private static int SERVER_PORT = 7889;
+    private Thread thread;
+    private String ipAddress;
+    private UDPDataPackage udpDataPackage;
+    private Handler handler;
+
+
+    class SendData implements Runnable {
+        @Override
+        public void run() {
+            try {
+                Socket socket = new Socket(ipAddress, SERVER_PORT);
+                socket.setReuseAddress(true);
+                socket.setKeepAlive(true);
+
+                OutputStream outputStream = socket.getOutputStream();
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+                objectOutputStream.writeObject(udpDataPackage);
+
+                InputStream inputStream = socket.getInputStream();
+                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+                DataPackage dataPackage = (DataPackage) objectInputStream.readObject();
+
+                Message msg = new Message();
+                msg.what = SERVER_PORT;
+                msg.obj = dataPackage;
+                handler.sendMessage(msg);
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sendRequest(String ipAddress, UDPDataPackage udpDataPackage, Handler handler) {
+        this.ipAddress = ipAddress;
+        this.udpDataPackage = udpDataPackage;
+        this.handler = handler;
+        thread = new Thread(new SendData());
+    }
+}
---
+++ app/src/main/java/xyz/viseator/anonymouscard/network/TcpServer.java
@@ -0,0 +1,74 @@
+package xyz.viseator.anonymouscard.network;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import xyz.viseator.anonymouscard.data.DataPackage;
+import xyz.viseator.anonymouscard.data.DataStore;
+import xyz.viseator.anonymouscard.data.UDPDataPackage;
+
+/**
+ * Created by viseator on 2016/12/23.
+ * Wudi
+ * viseator@gmail.com
+ */
+
+public class TcpServer {
+    private static int SERVER_PORT = 7889;
+    private DataStore dataStore;
+    private Thread thread;
+
+    public TcpServer(DataStore dataStore) {
+        this.dataStore = dataStore;
+    }
+
+    class RunServer implements Runnable {
+        @Override
+        public void run() {
+            ServerSocket serverSocket = null;
+            try {
+                serverSocket = new ServerSocket();
+                serverSocket.setReuseAddress(true);
+                serverSocket.bind(new InetSocketAddress(SERVER_PORT));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            while (true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    InputStream inputStream;
+                    inputStream = socket.getInputStream();
+                    ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+                    UDPDataPackage udpDataPackage = (UDPDataPackage) objectInputStream.readObject();
+                    DataPackage dataPackage = dataStore.getDataById(udpDataPackage.getId());
+
+                    OutputStream outputStream = socket.getOutputStream();
+                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+                    objectOutputStream.writeObject(dataPackage);
+                    objectOutputStream.flush();
+
+                    objectOutputStream.close();
+                    outputStream.close();
+                    objectInputStream.close();
+                    inputStream.close();
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }
+    }
+
+    public void startServer() {
+        thread = new Thread(new RunServer());
+        thread.start();
+    }
+}
